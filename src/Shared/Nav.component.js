c44193487948eb5c834c89a7bdd77c3f121ccedb
Jose Antonio Dominguez <chamot11@gmail.com>
2020-05-24 12:09:39 -0300

    Ally fix

--- src/Shared/Nav.component.js
+++ src/Shared/Nav.component.js
@@ -8,7 +8,7 @@ export const Nav = () => {
   return (
     <div className="z-10 bg-white w-full h-16 shadow-xs fixed">
       <div className="flex text-gray-700 w-full h-full max-w-screen-xl m-auto">
-        <a href="#" className="h-full flex items-center sm:pr-4">
+        <a href="#home" className="h-full flex items-center sm:pr-4">
           <img id="logo" src={logo} alt="Pizza Task" className="p-4 h-full" />
           <h1 className="text-xl py-4">Pizza Task</h1>
         </a>
