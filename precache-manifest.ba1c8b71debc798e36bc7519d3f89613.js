10833b96bf18a247c843fabd6167ff85c9af370c
Jose Antonio Dominguez <chamot11@gmail.com>
2020-05-27 11:10:24 -0300

    Updates

---
+++ precache-manifest.ba1c8b71debc798e36bc7519d3f89613.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d58ef97865e0dcf5bf06eccb737d51ad",
+    "url": "/the-pizza-task-fe/index.html"
+  },
+  {
+    "revision": "8ff947d10e00b4ee7832",
+    "url": "/the-pizza-task-fe/static/css/main.13ac223e.chunk.css"
+  },
+  {
+    "revision": "2bf5e4b64734556253b2",
+    "url": "/the-pizza-task-fe/static/js/2.dbf0d90f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/the-pizza-task-fe/static/js/2.dbf0d90f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ff947d10e00b4ee7832",
+    "url": "/the-pizza-task-fe/static/js/main.90c87d7a.chunk.js"
+  },
+  {
+    "revision": "bbe7b4c90dd78fb55fca",
+    "url": "/the-pizza-task-fe/static/js/runtime-main.5dea1ee5.js"
+  },
+  {
+    "revision": "131ee69e1ae5acd4e584cb9603797797",
+    "url": "/the-pizza-task-fe/static/media/cart.131ee69e.svg"
+  },
+  {
+    "revision": "9a47dba4a4f5d2f1401c0e76dbcf2187",
+    "url": "/the-pizza-task-fe/static/media/logo.9a47dba4.png"
+  }
+]);
